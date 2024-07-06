43a15e42907c4d5dccc26f3c4e1ac47f541cab82
Juan Diego Velasco <juandiegovelsol@gmail.com>
2024-07-06 00:08:20 -0500

    Initial Commit

---
+++ BeeTask2/BeeTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  BeeTask2App.swift
+//  BeeTask2
+//
+//  Created by Angela maria Erazo lozano on 6/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BeeTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeeTask2UITests/BeeTask2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BeeTask2UITestsLaunchTests.swift
+//  BeeTask2UITests
+//
+//  Created by Angela maria Erazo lozano on 6/07/24.
+//
+
+import XCTest
+
+final class BeeTask2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
